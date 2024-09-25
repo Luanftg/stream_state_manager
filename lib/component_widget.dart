39f3ecaf19be7a3f2afee070a7ed98ef4f34ae18
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2024-09-24 22:42:10 -0300

    Update component_widget.dart
    fix call

--- lib/component_widget.dart
+++ lib/component_widget.dart
@@ -34,7 +34,7 @@ class ComponentWidget<T extends Component, ComponentState>
             case ConnectionState.waiting:
             case ConnectionState.active:
                  WidgetsBinding.instance.addPostFrameCallback((_) =>
-                onListen?.call();
+                onListen?.call()
               );
               return builder.call(snapshot.data as ComponentState);
             case ConnectionState.done:
